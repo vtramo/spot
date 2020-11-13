d6994c422c2a158a74f7598d6c4fe347fab79c02
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2020-11-13 18:46:25 +0100

    space before ; style patch

--- spot/ltsmin/spins_interface.hh
+++ spot/ltsmin/spins_interface.hh
@@ -60,9 +60,9 @@ namespace spot
     int (*get_type_value_count)(int type);
     const char* (*get_type_value_name)(int type, int value);
     // label based functions
-    int (*eval_state_label) (int label, const int* src) ;
-    int (*get_state_label_count) () ;
-    const char * (*get_state_label_name) (int label) ;
+    int (*eval_state_label) (int label, const int* src);
+    int (*get_state_label_count) ();
+    const char * (*get_state_label_name) (int label);
   private:
     // handle to the dynamic library. The variable is of type lt_dlhandle, but
     // we need this trick since we cannot put ltdl.h in public headers
