2a4e68bfae3c5a93f37c4f3ceb3cd23d3deee8e9
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2022-05-17 17:43:27 +0200

    more noexcept to pleace GCC
    * spot/bricks/brick-hashset (Row): Add noexcept.
    * bin/autcross.cc (out_statistics): Likewise.
    * bin/ltlcross.cc (statistics): Likewise.

--- bin/autcross.cc
+++ bin/autcross.cc
@@ -345,7 +345,6 @@ struct in_statistics
 
 struct out_statistics
 {
-
   // If OK is false, output statistics are not available.
   bool ok;
   const char* status_str;
@@ -353,7 +352,7 @@ struct out_statistics
   double time;
   aut_statistics output;
 
-  out_statistics()
+  out_statistics() noexcept
     : ok(false),
       status_str(nullptr),
       status_code(0),
