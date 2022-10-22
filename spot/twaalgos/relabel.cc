26b08c99dfb82739179d253f17d30d42c041cbe3
Philipp Schlehuber-Caissier <philipp@lrde.epita.fr>
2022-10-22 16:34:23 +0200

    first version

--- spot/twaalgos/relabel.cc
+++ spot/twaalgos/relabel.cc
@@ -20,9 +20,152 @@
 #include "config.h"
 #include <spot/twaalgos/relabel.hh>
 #include <spot/twa/formula2bdd.hh>
+#include <spot/tl/formula.hh>
+
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
 
 namespace spot
 {
+  namespace
+  {
+    struct bdd_partition
+    {
+      using pou = std::array<unsigned, 2>;
+      std::vector<bdd> treated;
+      std::vector<pou> offsprings;
+      std::vector<pou> offsprings_list;
+      std::vector<formula> new_aps;
+      bool relabel_succ = false;
+    };
+
+    bdd_partition
+    try_partition_me(const std::vector<bdd>& all_cond, unsigned max_letter)
+    {
+      // We create vector that will be succesively filled.
+      // Each entry corresponds to a "letter", of the partition
+      bdd_partition result;
+
+      const size_t Norig = all_cond.size();
+      auto& treated = result.treated;
+      treated.reserve(Norig);
+      auto& offsprings = result.offsprings;
+      offsprings = std::vector<bdd_partition::pou>(Norig, {-1u,-1u});
+      auto& offsprings_list = result.offsprings_list;
+      offsprings_list.reserve(Norig);
+
+      auto add_idx = [&](unsigned old_cond_idx, unsigned letter_idx)
+        {
+          unsigned tail = offsprings[old_cond_idx][1];
+          unsigned oIdx = offsprings_list.size();
+          offsprings_list.push_back({letter_idx, -1u});
+          if (tail == -1u)
+            offsprings[old_cond_idx] = {oIdx, oIdx};
+          else
+            {
+              offsprings_list[tail][1] = oIdx;
+              offsprings[old_cond_idx][1] = oIdx;
+            }
+        };
+
+      // Partition while keeping track who is an ancestor
+      // Also change to handmade linked list?
+      std::vector<std::pair<bdd, std::vector<unsigned>>> treated_inter;
+      treated_inter.reserve(Norig);
+
+      for (unsigned io = 0; io < Norig; ++io)
+        {
+          bdd cond = all_cond[io];
+          const auto Nt = treated_inter.size();
+          for (size_t in = 0; in < Nt; ++in)
+            {
+              if (cond == bddfalse)
+                break;
+              if (treated_inter[in].first == cond)
+                {
+                  // Found this very condition -> done
+                  treated_inter[in].second.push_back(io);
+                  cond = bddfalse;
+                  break;
+                }
+              if (bdd_have_common_assignment(treated_inter[in].first, cond))
+                {
+                  bdd inter = treated_inter[in].first & cond;
+                  treated_inter[in].first -= inter;
+                  cond -= inter;
+                  treated_inter.emplace_back(std::make_pair(
+                                             inter,
+                                             treated_inter[in].second));
+                  treated_inter.back().second.push_back(io);
+                  if (treated_inter.size() > max_letter)
+                    return bdd_partition{};
+                }
+            }
+            if (cond != bddfalse)
+              treated_inter.emplace_back(std::make_pair(
+                                         cond,
+                                         std::vector<unsigned>{io}));
+        }
+
+      // Redistribute
+      const size_t Nnc = treated_inter.size();
+      treated.reserve(Nnc);
+      for (size_t i = 0; i < Nnc; ++i)
+        {
+          std::cout << i << " : " << treated_inter[i].first << '\n';
+          treated.push_back(treated_inter[i].first);
+          for (auto o : treated_inter[i].second)
+            add_idx(o, i);
+        }
+      result.relabel_succ = true;
+      return result;
+    }
+
+    std::vector<bdd>
+    comp_new_letters(bdd_partition& part,
+                    twa_graph& aut,
+                    const std::string& var_prefix)
+    {
+      const auto& treated = part.treated;
+      auto& new_aps = part.new_aps;
+      // Get the new variables and their negations
+      const unsigned Nnl = treated.size();
+      const unsigned Nnv = std::ceil(std::log2(Nnl));
+      std::vector<std::array<bdd, 2>> Nv_vec(Nnv);
+
+      new_aps.reserve(Nnv);
+      for (unsigned i = 0; i < Nnv; ++i)
+        {
+          // todo check if it does not exist / use anonymous?
+          new_aps.push_back(formula::ap(var_prefix+std::to_string(i)));
+          int v = aut.register_ap(new_aps.back());
+          Nv_vec[i] = {bdd_nithvar(v), bdd_ithvar(v)};
+        }
+
+      std::vector<bdd> treated_newcond(treated.size());
+
+      for (unsigned i = 0; i < Nnl; ++i)
+        {
+          unsigned c = 0;
+          unsigned rem = i;
+          bdd thisbdd = bddtrue;
+          while (rem)
+            {
+              thisbdd &= Nv_vec[c][rem & 1];
+              ++c;
+              rem >>= 1;
+            }
+          for (; c < Nnv; ++c)
+            thisbdd &= Nv_vec[c][0];
+          treated_newcond[i] = thisbdd;
+        }
+      return treated_newcond;
+    }
+  }
+
+
   void
   relabel_here(twa_graph_ptr& aut, relabeling_map* relmap)
   {
@@ -95,5 +238,162 @@ namespace spot
         aut->merge_edges();     // remove any edge labeled by 0
         aut->purge_dead_states(); // remove useless states
       }
+  }
+
+  partition_relabel_dict::partition_relabel_dict(const std::vector<bdd>&
+                                                    old_letters,
+                                                 const std::vector<bdd>&
+                                                    new_letters,
+                                                  std::vector<formula>
+                                                    new_aps)
+    : relabel_succ_{true}
+    , new_aps_(std::move(new_aps))
+  {
+    if (old_letters.size() != new_letters.size())
+      throw std::runtime_error("Letters need to be bijective, "
+                               "trivially non-verified");
+    const auto N = old_letters.size();
+    orig_letters_.reserve(N);
+    for (size_t i = 0; i < N; ++i)
+      orig_letters_[new_letters[i]] = old_letters[i];
+  }
+
+  bdd partition_relabel_dict::compute_(const bdd& new_cond)
+  {
+    // todo test if new_cond is a disjunction of new_letters?
+    // Otherwise this is incorrect
+    bdd old_cond = bddfalse;
+    for (const auto& [k,v] : orig_letters_)
+      {
+        if (bdd_implies(k, new_cond))
+          old_cond |= v;
+      }
+    if (store_results_)
+      computed_conditions_[new_cond] = old_cond;
+    return old_cond;
+  }
+
+  // todo swig and std::optional?
+  partition_relabel_dict
+  try_partitioned_relabel_here(twa_graph_ptr& aut,
+                               bool split,
+                               unsigned max_letter,
+                               unsigned max_letter_mult,
+                               std::function<bool(unsigned)> select_states,
+                               std::string var_prefix)
+  {
+    auto abandon = []()
+      {
+        return partition_relabel_dict();
+      };
+
+    if (split)
+      {
+        aut->get_graph().remove_dead_edges_();
+        aut->get_graph().sort_edges_();
+        aut->get_graph().chain_edges_();
+      }
+
+
+    // Get all conditions present in the automaton
+    std::vector<bdd> all_cond;
+    bdd ignoredcon = bddtrue;
+    std::unordered_map<int, unsigned> all_cond_id2idx;
+
+    all_cond.reserve(0.1*aut->num_states());
+    all_cond_id2idx.reserve(all_cond.size());
+
+    for (const auto& e : aut->edges())
+      if (!select_states || select_states(e.src))
+        {
+          auto [_, ins] =
+              all_cond_id2idx.try_emplace(e.cond.id(), all_cond.size());
+          if (ins)
+            all_cond.push_back(e.cond);
+        }
+      else
+        ignoredcon &= bdd_support(e.cond);
+
+    if (all_cond.size() > max_letter)
+      return abandon();
+
+    const unsigned stop =
+        std::min(max_letter,
+                 (unsigned) (max_letter_mult*all_cond.size()));
+
+    auto this_partition = try_partition_me(all_cond, stop);
+
+    if (!this_partition.relabel_succ)
+      return abandon();
+
+    std::vector<bdd> new_letters =
+      comp_new_letters(this_partition, *aut, var_prefix);
+
+    //auto& treated = this_partition->treated;
+    auto& offsprings = this_partition.offsprings;
+    auto& offsprings_list = this_partition.offsprings_list;
+
+    // Replace all treated transitions in the original automaton
+    // with all offsprings in the new variables
+    if (split)
+      {
+        // All edges are sorted, no dead edges etc
+        const unsigned Neold = aut->num_edges();
+        for (unsigned e_idx = 1; e_idx <= Neold; ++e_idx)
+          {
+            auto& e = aut->edge_storage(e_idx);
+
+            unsigned idx = all_cond_id2idx[e.cond.id()];
+            unsigned of_idx = offsprings[idx][0];
+            e.cond = new_letters[offsprings_list[of_idx][0]];
+            of_idx = offsprings_list[of_idx][1];
+            while (of_idx != -1u)
+              {
+                unsigned of = offsprings_list[of_idx][0];
+                aut->new_edge(e.src, e.dst,
+                              new_letters.at(of), e.acc);
+                of_idx = offsprings_list[of_idx][1];
+              }
+          }
+        return partition_relabel_dict(this_partition.treated,
+                                      new_letters, this_partition.new_aps);
+      }
+    else
+      {
+        const size_t Nc = all_cond.size();
+        // Relabel edges by disjunctions over all offsprings
+        auto resdict =
+          partition_relabel_dict(this_partition.treated, new_letters,
+                                 this_partition.new_aps);
+        auto& cond_map_n2o = resdict.computed_conditions_;
+        cond_map_n2o.reserve(Nc);
+
+        // Translate all (old) conditions into new_ones
+        std::vector<bdd> new_all_cond(Nc);
+        for (size_t i = 0; i < Nc; ++i)
+          {
+            unsigned of_idx = offsprings[i][0];
+            bdd new_cond = new_letters[offsprings_list[of_idx][0]];
+            of_idx = offsprings_list[of_idx][1];
+            while (of_idx != -1u)
+              {
+                unsigned of = offsprings_list[of_idx][0];
+                new_cond |= new_letters[of];
+                of_idx = offsprings_list[of_idx][1];
+              }
+            new_all_cond[i] = new_cond;
+            cond_map_n2o[new_cond] = all_cond[i];
+          }
+
+        const unsigned Nstates = aut->num_states();
+        for (unsigned n = 0; n < Nstates; ++n)
+          {
+            if (!select_states || select_states(n))
+              for (auto& e : aut->out(n))
+                e.cond = new_all_cond[all_cond_id2idx[e.cond.id()]];
+          }
+        return resdict;
+      }
+    SPOT_UNREACHABLE();
   }
 }
--- spot/twaalgos/relabel.hh
+++ spot/twaalgos/relabel.hh
@@ -21,6 +21,10 @@
 
 #include <spot/tl/relabel.hh>
 #include <spot/twa/twagraph.hh>
+#include <spot/misc/bddlt.hh>
+
+#include <optional>
+#include <functional>
 
 namespace spot
 {
@@ -33,4 +37,106 @@ namespace spot
   /// or relabel_bse().
   SPOT_API void
   relabel_here(twa_graph_ptr& aut, relabeling_map* relmap);
+
+  class partition_relabel_dict;
+
+  /// \brief Replace conditions in \a aut with non-overlapping conditions
+  /// over fresh variables.
+  ///
+  /// Partitions the conditions in the automaton, then (binary) encodes
+  /// them using fresh propositions.
+  /// This can lead to an exponential explosion in the number of
+  /// conditions. The operations is aborted if either
+  /// the number of new letters exceeds \a max_letter
+  /// OR \a max_letter_mult times the number of conditions in the
+  /// original automaton
+  /// The argument \a select_states can be used to filter out states.
+  /// If given, only the transitions leaving states verifying
+  /// select_states are taken into account.
+  /// All transitions leaving states for whichselect_states
+  /// returns false are simply copied over
+  /// \note Select states should be used with care, and mostly makes
+  /// sense for synthesis to distinguish player and env states
+  SPOT_API partition_relabel_dict
+  try_partitioned_relabel_here(twa_graph_ptr& aut,
+                               bool split = false,
+                               unsigned max_letter = -1u,
+                               unsigned max_letter_mult = -1u,
+                               std::function<bool(unsigned)> select_states
+                                 = std::function<bool(unsigned)>(),
+                               std::string var_prefix = "__nv");
+
+  class SPOT_API partition_relabel_dict
+  {
+    friend partition_relabel_dict
+              try_partitioned_relabel_here(twa_graph_ptr& aut,
+                                           bool split,
+                                           unsigned max_letter,
+                                           unsigned max_letter_mult,
+                                           std::function<bool(unsigned)>
+                                               select_states,
+                                           std::string car_prefix);
+  public:
+    // Data structure used to map letters
+    using l_map = std::unordered_map<bdd, bdd, bdd_hash>;
+  private:
+    // The original letters used to relabel the automaton
+    l_map orig_letters_;
+    // Disjunction of original letters whose images have already been
+    // computed
+    l_map computed_conditions_;
+    // If true, computed results will be stored and can be reused
+    // can speed up computation but needs more memory
+    // True by default
+    bool store_results_ = true;
+
+    // Whether or not relabeling took place
+    bool relabel_succ_ = false;
+
+    // The new variables
+    std::vector<formula> new_aps_;
+
+    bdd compute_(const bdd& new_cond);
+  public:
+    partition_relabel_dict() = default;
+    partition_relabel_dict(const std::vector<bdd>& old_letters,
+                           const std::vector<bdd>& new_letters,
+                           std::vector<formula> new_aps);
+
+    void store_results(bool do_store)
+    {
+      store_results_ = do_store;
+    }
+
+    bool succ() const
+    {
+      return relabel_succ_;
+    }
+
+    const std::vector<formula>&
+    new_aps() const
+    {
+      return new_aps_;
+    }
+
+    /// \brief Takes a condition over the new variables
+    /// \a new_cond and return the corresponding condition
+    /// over the old variables
+    bdd translate(const bdd& new_cond)
+    {
+      SPOT_ASSERT(relabel_succ_);
+      // First search in orig, then precomputed
+      for (const auto& map : {orig_letters_, computed_conditions_})
+        {
+          auto it = map.find(new_cond);
+          if (it != map.end())
+            return it->second;
+        }
+
+      // Compute
+      return compute_(new_cond);
+    }
+    // todo Do we need/want the other direction
+  };
+
 }
