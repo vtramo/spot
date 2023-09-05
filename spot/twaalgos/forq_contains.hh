28a6471efbae8ebea5568bb650f0c4d3b4501168
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2023-09-05 11:39:13 +0200

    forq: fix bib entry and bind the doxygen doc to a group
    * doc/spot.bib: Reformat the FORQ reference in the style of the
    rest of the bibliographic file.
    * spot/twaalgos/forq_contains.hh: Adjust, and add missing \ingroup.

--- spot/twaalgos/forq_contains.hh
+++ spot/twaalgos/forq_contains.hh
@@ -23,29 +23,31 @@
 
 namespace spot
 {
+  /// \ingroup containment
   /// \brief Returns a word accepted by \a left that is rejected by \a right,
   /// or nullptr.
   ///
-  /// This implements the language containment algorithm from
-  /// \cite{doveriFORQBasedLanguageInclusion2022}
-  /// to check whether L(left)⊆L(right), in which case, it returns nullptr.
-  /// Otherwise, it returns a counterexample, i.e., a word that is accepted
-  /// by $L(left)\setminus L(right)$, hence the name of the function.
+  /// This implements a FORQ-based language containment algorithm
+  /// \cite doveri.22.cav to check whether L(left)⊆L(right), in which
+  /// case, it returns nullptr.  Otherwise, it returns a
+  /// counterexample, i.e., a word that is accepted by
+  /// $L(left)\setminus L(right)$, hence the name of the function.
   ///
-  /// \pre Automata \a left and \a right should be
-  /// non-alternating state-based Büchi-automata.
-  SPOT_API twa_word_ptr difference_word_forq(
-    const_twa_graph_ptr left, spot::const_twa_graph_ptr right);
+  /// \pre Automata \a left and \a right should be non-alternating
+  /// Büchi-automata.
+  SPOT_API
+  twa_word_ptr difference_word_forq(const_twa_graph_ptr left,
+                                    const_twa_graph_ptr right);
 
+  /// \ingroup containment
   /// \brief Returns a boolean value indicating
   /// whether \a left is included in the language of \a right.
   ///
-  /// This implements the language containment algorithm from
-  /// \cite{doveriFORQBasedLanguageInclusion2022}
-  /// to check whether L(left)⊆L(right).
+  /// This implements a FORQ-based language containment algorithm
+  /// to check whether L(left)⊆L(right). \cite doveri.22.cav
   ///
-  /// \pre Automata \a left and \a right should be
-  /// non-alternating state-based Büchi-automata.
-  SPOT_API bool contains_forq(
-    const_twa_graph_ptr left, const_twa_graph_ptr right);
+  /// \pre Automata \a left and \a right should be non-alternating
+  /// Büchi-automata.
+  SPOT_API
+  bool contains_forq(const_twa_graph_ptr left, const_twa_graph_ptr right);
 }
